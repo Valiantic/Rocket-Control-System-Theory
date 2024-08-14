5de6e47458fa8dcb43bf7f1c0c8446c6f2e6965c
StevenMadali <stevenmadali17@gmail.com>
2024-08-14 22:49:33 +0800

    designing a pid controller

--- main.py
+++ main.py
@@ -10,4 +10,10 @@ den = [2, 2, 1]
 # constructs the transfer function
 G = ctrl.TransferFunction(num, den)
 
+# PID controller (proportional, integral, derivative)
+
+Kp = 5 # sets proportiinal gain to 5
+Ki = 2 # sets integral gain to 2 
+Kd = 1 # sets derivative gain to 1 
+C = ctrl.TransferFunction([Kd, Kp, Ki], [1,0])
 
